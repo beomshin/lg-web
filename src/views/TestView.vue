eedfe3fe35fb8d99d027845a01b16f9f796d0d3f
beomshin.kim <chzh859@naver.com>
2023-01-04 23:16:11 +0900

    # 테스트 페이지 생성

---
+++ src/views/TestView.vue
@@ -0,0 +1,19 @@
+<template>
+  <test-header />
+  <test-body/>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import TestHeader from '@/components/test/TestHeader.vue'
+import TestBody from '@/components/test/TestBody.vue'
+
+@Options({
+  components: {
+    TestHeader,
+    TestBody
+  }
+})
+export default class HomeView extends Vue {
+}
+</script>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer : {
+    proxy : {
+      '/law': {
+        target : 'http://localhost:19001',
+        changeOrigin: true,
+      }
+    }
+  }
 })
